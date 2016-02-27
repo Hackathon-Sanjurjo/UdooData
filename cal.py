cd87bec96825b9690f28bf0af9ac1ab8cdb67f8d
Clara C <clara.casas.castedo@gmail.com>
2016-02-28 03:39:49 +0900

    getting acceleration data, new configuration parameters

--- cal.py
+++ cal.py
@@ -1,9 +1,9 @@
 import magum
 import pickle 
 
-magum = magum.Magum(2000,0,2,1)
+magum = magum.Magum(250,0,4,1)
 
-# Calibrating the sensors and saving them to fcal file.
+# Calibrating the sensors and saving them to cal file.
 cal_sens = magum.calibrateSens(1000)
 print(cal_sens)
 pickle.dump(cal_sens, open('cal','wb'))
--- sensors.py
+++ sensors.py
@@ -7,30 +7,33 @@ import time
 #Initialization
 ###############
 #Sensors object
-magum = magum.Magum(2000,0,2,1)
+# gyroscope: 250 gps, accelerometer:+/- 4g 
+magum = magum.Magum(250,0,4,1)
 
 #Zmq-communications
 context = zmq.Context()
 publisher = context.socket(zmq.PUB)
 publisher.bind('tcp://10.42.0.114:9876')
 
+##############
+#Calculations
+##############
 #The calibration values are calculated with 1000 samples in the
-#file 'cal'. calibrateSens function also run for internal reasons.
+#file 'cal'. calibrateSens function is also invoked for internal library 
+#reasons.
 
 cal_sens = magum.calibrateSens(1)
 cal_sens = pickle.load(open('cal','rb'))
-
-
-#Read angle values for x[0], y[1], z[2] axes and send data with zmq.
 cal_acc_gyr = cal_sens[0:6]
-dt = 0.1
+dt = 0.05
 
 while True:
+	# Get Data: degrees and acceleration
 	angles_array = magum.compFilter(dt,cal_acc_gyr)
-	compFiltObj = (time.time(),angles_array)
-	publisher.send_pyobj(compFiltObj)
-	print 'x {}'.format(angles_array[0])
-	print 'y {}'.format(angles_array[1])
-	print 'z {}'.format(angles_array[2])
-
-
+	accel_array = magum.readAData('raw') #g force unit
+	
+	# Send data
+	sensorObj = (time.time(),angles_array, accel_array)	
+	publisher.send_pyobj(sensorObj)
+	print 'x accel {}'.format(accel_array[0])
+	print 'x comp {}'.format(angles_array[0])
